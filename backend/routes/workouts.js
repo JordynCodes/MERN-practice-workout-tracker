458ae44fd9e656d3a387e440369467a7586ee70e
Jordyn Lally <codingwithjordyn@gmail.com>
2024-03-01 21:33:34 -0500

    basic workout routes

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -7,4 +7,24 @@ router.get('/', (req, res) => {
     res.json({mssg: 'GET all workouts'})
 })
 
+// get a single workout
+router.get('/:id', (req, res) => {
+    res.json({mssg: 'GET a single workout'})
+})
+
+// create new workout
+router.post('/', (req, res) => {
+    res.json({mssg: 'POST a new workout'})
+})
+
+// delete workout
+router.delete('/:id', (req, res) => {
+    res.json({mssg: 'DELETE a workout'})
+})
+
+// update workout
+router.patch('/', (req, res) => {
+    res.json({mssg: 'UPDATE a workout'})
+})
+
 module.exports = router;
