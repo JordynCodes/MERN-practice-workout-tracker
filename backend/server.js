7e6fbfce3d044db48bc8a55c37690c67606542c1
Jordyn Lally <codingwithjordyn@gmail.com>
2024-03-02 19:10:29 -0500

    display all workouts

--- backend/server.js
+++ backend/server.js
@@ -18,11 +18,5 @@ mongoose.connect(process.env.MONGO_URI)
 // middleware
 app.use(express.json());
 
-app.use((req, res, next) => {
-    console.log(req.path, req.method);
-    next();
-})
-
 // routes
-
 app.use('/api/workouts', workoutRoutes);
