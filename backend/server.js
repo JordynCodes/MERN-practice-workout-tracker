e2835117797f106d20361f0d0d0fba024a8f90aa
Jordyn Lally <codingwithjordyn@gmail.com>
2024-03-04 04:53:24 -0500

    user routing/controller/model

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 // setting up express app
 const app = express();
 const workoutRoutes = require('./routes/workouts');
+const userRoutes = require('./routes/user');
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
@@ -20,3 +21,4 @@ app.use(express.json());
 
 // routes
 app.use('/api/workouts', workoutRoutes);
+app.use('/api/user', userRoutes);
