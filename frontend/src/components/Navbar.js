aab99441af75057f2e76b61baee6f9476b97bcbe
Jordyn Lally <codingwithjordyn@gmail.com>
2024-03-06 00:42:23 -0500

    login and signup forms

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,6 +7,16 @@ const Navbar = () => {
                 <Link to="/">
                     <h1>Workout Buddy</h1>
                 </Link>
+                <nav>
+                    <div>
+                        <Link to="/login">
+                            <h1>Log in</h1>
+                        </Link>
+                        <Link to="/signup">
+                            <h1>Sign up</h1>
+                        </Link>
+                    </div>
+                </nav>
             </div>
         </header>
      );
